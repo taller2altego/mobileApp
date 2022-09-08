9ebd47fad4b42040997aff7735da75bc886d1cf1
IAvecilla <iavecilla@fi.uba.ar>
2022-09-08 01:37:25 -0300

    Agrego nueva configuracion de la shell de nix
    - Nuevas dependencias relacionadas a herramientas de desarrollo de android

--- shell.nix
+++ shell.nix
@@ -1,11 +1,28 @@
-{ pkgs ? import <nixpkgs> {config.android_sdk.accept_license = true;} }:
+{ pkgs ? import <nixpkgs> { } }:
 
 with pkgs;
 
+let
+  android-nixpkgs = callPackage (import (builtins.fetchGit {
+    url = "https://github.com/tadfisher/android-nixpkgs.git";
+  })) {
+    channel = "stable";
+  };
+  
+  android-sdk = android-nixpkgs.sdk (sdkPkgs: with sdkPkgs; [
+    cmdline-tools-latest
+    build-tools-32-0-0
+    platform-tools
+    platforms-android-31
+    emulator
+  ]);
+
+in
 mkShell {
-  buildInputs = [ nodejs git];
-  shellHook = '' 
-    export ANDROID_SDK_ROOT=$HOME/Library/Android/Sdk
-    export PATH=$PATH:~/.npm-global/bin:$ANDROID_SDK_ROOT/emulator:$ANDROID_SDK_ROOT/platform-tools
-  '';
+  buildInputs = [
+    android-studio
+    android-sdk
+    nodejs
+    git
+  ];
 }
