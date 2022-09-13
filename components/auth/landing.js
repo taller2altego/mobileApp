2bcc1c8c294456b4127bed01044034b81a7134e4
IAvecilla <iavecilla@fi.uba.ar>
2022-09-13 03:02:12 -0300

    Creo componentes de registro, login y landing

---
+++ components/auth/landing.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Text, View, Button, Pressable } from "react-native";
+
+import styles from "../styles";
+
+export default function Landing({ navigation }) {
+  return (
+    <View style={styles.centered}>
+      <Pressable
+        style={styles.regButton}
+        onPress={() => navigation.navigate("Register")}
+      >
+        <Text style={styles.textButton}> Register </Text>
+      </Pressable>
+      <Pressable
+        style={styles.regButton}
+        title="Login"
+        onPress={() => navigation.navigate("Login")}
+      >
+        <Text style={styles.textButton}> Login </Text>
+      </Pressable>
+    </View>
+  );
+}
