bd9b10f28d3a2ad5e530afc76de65716a6a72235
IAvecilla <iavecilla@fi.uba.ar>
2022-09-13 03:02:36 -0300

    Creo archivo de estilos para utilizar en la app

---
+++ components/styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  centered: {
+    flex: 1,
+    justifyContent: "flex-end",
+  },
+  regButton: {
+    padding: 8,
+    margin: 8,
+    backgroundColor: "purple",
+    //borderRadius: "10px",
+  },
+  textButton: {
+    textAlign: "center",
+    color: "white",
+  },
+});
+
+export default styles;
