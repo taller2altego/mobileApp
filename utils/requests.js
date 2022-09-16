46a791fb9fb00268e2d9a982ffba31029374abdf
IAvecilla <iavecilla@fi.uba.ar>
2022-09-16 02:01:21 -0300

    Formateo y eliminacion de archivos no utilizados

--- utils/requests.js
+++ utils/requests.js
@@ -1,14 +1,15 @@
 import axios from "axios";
 
 const post = (url, body, extraHeaders) => {
-  return axios.post(url, body, { headers: { ...extraHeaders, 'Content-Type': 'application/json' } });
+  return axios.post(url, body, {
+    headers: { ...extraHeaders, "Content-Type": "application/json" },
+  });
 };
 
-const get = (token, extraHeaders) => {
-  return axios.get(url, { headers: { ...extraHeaders, 'Authorization': `Bearer ${token}` } });
+const get = (url, token, extraHeaders) => {
+  return axios.get(url, {
+    headers: { ...extraHeaders, "Authorization": `Bearer ${token}` },
+  });
 };
 
-export {
-  post,
-  get
-}
+export { get, post };
