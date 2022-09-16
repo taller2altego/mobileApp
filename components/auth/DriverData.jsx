bd6869564591567c7bc738c2fc600c62fee91ad2
IAvecilla <iavecilla@fi.uba.ar>
2022-09-16 02:00:00 -0300

    Actualizacion de diferentes pantallas para login y registro

---
+++ components/auth/DriverData.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import { Button, Pressable, Text, TextInput, View } from "react-native";
+import { post } from "../../utils/requests";
+import styles from "../styles";
+
+export default function DriverData({navigation}) {
+  return (
+    <View>
+    </View>
+  );
+}
